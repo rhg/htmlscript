863adbd2e2c16ac6f92160cd55945f9dd884c3cd
Ricardo Gomez <ricardo.gomez331@gmail.com>
2014-04-14 14:15:26 -0500

    add cljsbuild

--- project.clj
+++ project.clj
@@ -3,5 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-cljsbuild "1.0.3"]]
+  :cljsbuild {:builds [{:source-paths ["server"]
+                        :compiler {:optimizations :simple
+                                   :target :nodejs
+                                   :output-to "server.js"}}]}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2197"]])
